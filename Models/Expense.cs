282fc122ee24d2e7ab6b16c2ed6f5d475b45c39d
Keerthikumar <bhashyamkeerthikumar@gmail.com>
2024-03-12 16:46:08 +0530

    Initial commit

---
+++ Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+using Microsoft.Build.Framework;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+        [DisplayName("Expense")]
+        [System.ComponentModel.DataAnnotations.Required]
+        public string ExpenseName { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Amount must be greater than 0!")]
+        public int Amount { get; set; }
+
+    }
+}
---
+++ Models/ExpenseType.cs
@@ -0,0 +1,14 @@
+﻿using MessagePack;
+using System.ComponentModel;
+
+namespace InAndOut.Models
+{
+    public class ExpenseType
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+        [DisplayName("Expense")]
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+    }
+}
